23e0ac484660cf508e00954da42c8f75f8a28b1f
solaire-off <victor374288@mail.ru>
2017-03-15 21:06:03 +0300

    Translate admin, add shop templates, create shop list view

--- mysite/settings.py
+++ mysite/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Minsk'
 
 USE_I18N = True
 
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import ShopItem, ShopCategory
 
-# Create your views here.
+def shop_list(request):
+	items = ShopItem.objects.all()
+	context = {
+	  'items':items,
+	}
+	return render (request,'shop/shop_list.html',context)
